540f94d8a62564bd18016150511c9f44adba1715
yhql <victor_servant@hotmail.com>
2021-01-13 16:17:22 +0100

    Use SDK's SyscallErrors

--- src/crypto_helpers.rs
+++ src/crypto_helpers.rs
@@ -1,13 +1,14 @@
 use nanos_sdk::bindings::*;
 use nanos_sdk::ecc::{CurvesId, DEREncodedECDSASignature};
+use nanos_sdk::io::SyscallError;
 
 pub const BIP32_PATH: [u32; 5] = nanos_sdk::ecc::make_bip32_path(b"m/44'/535348'/0'/0/0");
 
 /// Helper function that derives the seed over secp256k1
-pub fn bip32_derive_secp256k1(path: &[u32]) -> [u8; 32] {
+pub fn bip32_derive_secp256k1(path: &[u32]) -> Result<[u8; 32], SyscallError> {
     let mut raw_key = [0u8; 32];
-    nanos_sdk::ecc::bip32_derive(CurvesId::Secp256k1, path, &mut raw_key);
-    raw_key
+    nanos_sdk::ecc::bip32_derive(CurvesId::Secp256k1, path, &mut raw_key)?;
+    Ok(raw_key)
 } 
 
 /// Helper function that signs with ECDSA in deterministic nonce,
@@ -19,22 +20,13 @@ pub fn detecdsa_sign(m: &[u8], ec_k: &cx_ecfp_private_key_t) -> Result<(DEREncod
         m)
 }
 
-/// Helper function that verifies a signature produced with `detecdsa_sign`
-pub fn detecdsa_verify(m: &[u8], sig: &[u8], ec_pubk: &cx_ecfp_public_key_t) -> bool {
-    nanos_sdk::ecc::ecdsa_verify(&ec_pubk, 
-                sig, 
-          (CX_RND_RFC6979 | CX_LAST) as i32,
-       CX_SHA256,
-            &m)
-}
-
-pub fn get_pubkey() -> nanos_sdk::bindings::cx_ecfp_public_key_t {
-    let raw_key = bip32_derive_secp256k1(&BIP32_PATH);
-    let mut ec_k = nanos_sdk::ecc::ec_init_key(CurvesId::Secp256k1, &raw_key);
+pub fn get_pubkey() -> Result<nanos_sdk::bindings::cx_ecfp_public_key_t, SyscallError> {
+    let raw_key = bip32_derive_secp256k1(&BIP32_PATH)?;
+    let mut ec_k = nanos_sdk::ecc::ec_init_key(CurvesId::Secp256k1, &raw_key)?;
     nanos_sdk::ecc::ec_get_pubkey(CurvesId::Secp256k1, &mut ec_k)
 }
 
-pub fn get_private_key() -> nanos_sdk::bindings::cx_ecfp_private_key_t {
-    let raw_key = bip32_derive_secp256k1(&BIP32_PATH);
-    nanos_sdk::ecc::ec_init_key(CurvesId::Secp256k1, &raw_key)
+pub fn get_private_key() -> Result<nanos_sdk::bindings::cx_ecfp_private_key_t, SyscallError> {
+    let raw_key = bip32_derive_secp256k1(&BIP32_PATH)?;
+    Ok(nanos_sdk::ecc::ec_init_key(CurvesId::Secp256k1, &raw_key)?)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod utils;
 
 use nanos_sdk::buttons::ButtonEvent;
 use nanos_sdk::io;
-use nanos_sdk::ecc::{CurvesId, DEREncodedECDSASignature};
+use nanos_sdk::io::SyscallError;
+use nanos_sdk::ecc::DEREncodedECDSASignature;
 use nanos_ui::ui;
 use core::str::from_utf8;
 use crypto_helpers::*;
@@ -17,15 +18,20 @@ nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
 /// message scrollers
 fn show_pubkey() {
     let pubkey = get_pubkey();
-    {
-        let hex0 = utils::to_hex(&pubkey.W[1..33]).unwrap();
-        let m = from_utf8(&hex0).unwrap();
-        ui::MessageScroller::new(&m).event_loop();
-    }
-    {
-        let hex1 = utils::to_hex(&pubkey.W[33..65]).unwrap();
-        let m = from_utf8(&hex1).unwrap();
-        ui::MessageScroller::new(&m).event_loop();
+    match pubkey {
+        Ok(pk) => {
+            {
+                let hex0 = utils::to_hex(&pk.W[1..33]).unwrap();
+                let m = from_utf8(&hex0).unwrap();
+                ui::MessageScroller::new(&m).event_loop();
+            }
+            {
+                let hex1 = utils::to_hex(&pk.W[33..65]).unwrap();
+                let m = from_utf8(&hex1).unwrap();
+                ui::MessageScroller::new(&m).event_loop();
+            }
+        },
+        Err(_) => ui::popup("Error")
     }
 }
 
@@ -52,27 +58,19 @@ fn menu_example() {
 /// This is the UI flow for signing, composed of a scroller
 /// to read the incoming message, a panel that requests user
 /// validation, and an exit message.
-fn sign_ui(message: &[u8]) -> Result<Option<DEREncodedECDSASignature>, ()> {
+fn sign_ui(message: &[u8]) -> Result<Option<DEREncodedECDSASignature>, SyscallError> {
     ui::popup("Message review");
 
     {
-        let hex = utils::to_hex(&message)?;
-        let m = from_utf8(&hex).map_err(|_| ())?;
+        let hex = utils::to_hex(&message).map_err(|_| SyscallError::Overflow)?;
+        let m = from_utf8(&hex).map_err(|_| SyscallError::InvalidParameter)?;
 
         ui::MessageScroller::new(&m).event_loop();
     }
 
     if ui::Validator::new("Sign ?").ask() {
-        let mut k = get_private_key();
-        let (sig, sig_len) = detecdsa_sign(&message, &k).unwrap();
-
-        // Signature verification so we're sure the bindings are OK !
-        let pubkey = nanos_sdk::ecc::ec_get_pubkey(CurvesId::Secp256k1, &mut k);
-        if !detecdsa_verify(&message, &sig[..sig_len as usize], &pubkey) {
-            ui::popup("Invalid :(");
-            return Err(())
-        }
-
+        let k = get_private_key()?;
+        let (sig, _sig_len) = detecdsa_sign(&message, &k).unwrap();
         ui::popup("Done !");
         Ok(Some(sig))
     } else {
@@ -96,7 +94,7 @@ extern "C" fn sample_main() {
             io::Event::Command(ins) => {
                 match handle_apdu(&mut comm, ins) {
                     Ok(()) => comm.reply_ok(),
-                    Err(sw) => comm.reply(sw)
+                    Err(sw) => comm.reply(sw.into())
                 }
             }
             _ => ()
@@ -130,21 +128,21 @@ impl From<u8> for Ins {
     }
 }
 
+use nanos_sdk::io::Reply;
 
-fn handle_apdu(comm: &mut io::Comm, ins: Ins) -> Result<(), io::StatusWords> {
+fn handle_apdu(comm: &mut io::Comm, ins: Ins) -> Result<(), Reply> {
     if comm.rx == 0 {
-        return Err(io::StatusWords::NothingReceived)
+        return Err(io::StatusWords::NothingReceived.into())
     }
 
     match ins {
-        Ins::GetPubkey => comm.append(&get_pubkey().W), 
+        Ins::GetPubkey => comm.append(&get_pubkey()?.W),
         Ins::Sign => {
-            let out = sign_ui(comm.get_data()?)
-                            .map_err(|_| io::StatusWords::UserCancelled)?;
+            let out = sign_ui(comm.get_data()?)?;
             if let Some(o) = out { comm.append(&o) }
         }
         Ins::Menu => menu_example(),
-        Ins::ShowPrivateKey => comm.append(&bip32_derive_secp256k1(&BIP32_PATH)),
+        Ins::ShowPrivateKey => comm.append(&bip32_derive_secp256k1(&BIP32_PATH)?),
         Ins::SingleMessage => comm.append(&[0xb8; 32]),
         Ins::DoubleMessage => comm.append(&[0xf7; 64]),
         Ins::Exit => nanos_sdk::exit_app(0),

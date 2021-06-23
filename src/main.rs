bd4e83234bfe26c970508f7f0924778e260fcd0d
pscott <scott.piriou@ledger.fr>
2021-06-23 14:50:41 +0200

    Add working morpion

--- src/main.rs
+++ src/main.rs
@@ -7,61 +7,212 @@ use nanos_sdk::screen;
 
 nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
 
-fn screen_clear() {
-    screen::sdk_bagl_hal_draw_rect(0, 0, 0, 128, 64);
-    screen::sdk_screen_update();
+const GRID_SIZE: u8 = 9;
+
+// Could be enums ?
+const BLACK: u32 = 0;
+const WHITE: u32 = 1;
+
+#[derive(Clone, Copy, PartialEq)]
+enum Cell {
+    Empty,
+    Cross,
+    Nought,
 }
 
-fn screen_clear_and_draw(x_pos: i32, y_pos: i32, bmp: &[u8]) {
-    screen_clear();
-    screen::sdk_bagl_hal_draw_bitmap_within_rect(x_pos, y_pos, 128, 64, false, bmp);
-    screen::sdk_screen_update();
-}
+impl Cell {
+    fn draw(&self, cell_num: u8, selected: bool) {
+        let mut x = (32 + (cell_num % 3) * 21) as i32;
+        let mut y = ((cell_num / 3) * 21) as i32;
+        let color;
 
-fn calculate_new_position(x_pos: &mut i32, y_pos: &mut i32, ev: ButtonEvent) {
-
-    match ev {
-        ButtonEvent::RightButtonPress => {
-            *x_pos += 1;
-            *y_pos += 1;
-        },
-        ButtonEvent::LeftButtonPress => {
-            *x_pos -= 1;
-            *y_pos -= 1;
+        if selected {
+            screen::sdk_bagl_hal_draw_rect(WHITE, x, y, 19, 19);
+            color = BLACK;
+        } else {
+            screen::sdk_bagl_hal_draw_rect(BLACK, x, y, 19, 19);
+            color = WHITE;
         }
-        _ => ()
-    };
 
-   if *x_pos > 127 {
-       *x_pos = 0;
-   } else if *x_pos < 0 {
-       *x_pos = 127;
-   }
+        match &self {
+            Self::Empty => {}
+            Self::Cross => {
+                x += 6;
+                y += 19 / 2;
+                screen::sdk_bagl_hal_draw_rect(color, x, y, 8, 1);
+            }
+            Self::Nought => {
+                y += 6;
+                x += 19 / 2;
+                screen::sdk_bagl_hal_draw_rect(color, x, y, 1, 8);
+            }
+        }
+    }
+}
 
-   if *y_pos > 63 {
-       *y_pos = 0;
-   } else if *y_pos < 0 {
-       *y_pos = 63
-   }
+struct Grid {
+    cells: [Cell; 9],
+    turn: u8,
+    selected: u8,
+    // could use refs to bitmaps for noughts and crosses
+}
+
+impl Grid {
+    fn new() -> Self {
+        Self {
+            cells: [Cell::Empty; GRID_SIZE as usize],
+            turn: 0,
+            selected: 0,
+        }
+    }
+
+    fn draw(&self) {
+        screen::sdk_bagl_hal_draw_rect(BLACK, 0, 0, 128, 64);
+
+        // TOOD: draw borders ?
+
+        // draw columns
+        screen::sdk_bagl_hal_draw_rect(WHITE, 20 + 32, 0, 1, 64);
+        screen::sdk_bagl_hal_draw_rect(WHITE, 40 + 32, 0, 1, 64);
+
+        // draw lines
+        screen::sdk_bagl_hal_draw_rect(WHITE, 32, 20, 64, 1);
+        screen::sdk_bagl_hal_draw_rect(WHITE, 32, 40, 64, 1);
+        screen::sdk_screen_update();
+    }
+
+    /// Draw the mark (`Cross` or `Nought`). Does nothing if select cell is `Empty`.
+    fn add_mark(&mut self) {
+        // Increase selected due to weird behaviour with BothButtonRelease when a LeftButtonPress comes before...
+
+        let cell = &mut self.cells[self.selected as usize];
+        if *cell == Cell::Empty {
+            if self.turn % 2 == 0 {
+                *cell = Cell::Cross;
+            } else {
+                *cell = Cell::Nought;
+            }
+            self.turn += 1;
+        }
+        cell.draw(self.selected, true);
+        screen::sdk_screen_update();
+    }
+
+    fn select_next(&mut self) {
+        // Remove the highlight of currently selected cell.
+        let cell = self.cells[self.selected as usize];
+        cell.draw(self.selected, false);
+
+        if self.selected == GRID_SIZE - 1 {
+            self.selected = 0;
+        } else {
+            self.selected += 1;
+        }
+
+        // Draw it (highlighted)
+        let cell = self.cells[self.selected as usize];
+        cell.draw(self.selected, true);
+        screen::sdk_screen_update();
+    }
+
+    fn select_prev(&mut self) {
+        // Remove the highlight of currently selected cell.
+        let cell = self.cells[self.selected as usize];
+        cell.draw(self.selected, false);
+
+        // Select next cell
+        if self.selected == 0 {
+            self.selected = GRID_SIZE - 1;
+        } else {
+            self.selected -= 1;
+        }
+        // Draw it (highlighted)
+        let cell = self.cells[self.selected as usize];
+        cell.draw(self.selected, true);
+        screen::sdk_screen_update();
+    }
+
+    // Returns true if a row is complete.
+    fn check_rows(&self) -> bool {
+        for i in 0..3 {
+            let check = self.cells[i * 3];
+            if check == Cell::Empty {
+                continue;
+            }
+            if self.cells[i * 3 + 1..i * 3 + 3]
+                .iter()
+                .all(|&cell| cell == check)
+            {
+                return true;
+            }
+        }
+        false
+    }
+
+    // Returns true if a column is complete.
+    fn check_columns(&self) -> bool {
+        for i in 0..3 {
+            let check = self.cells[i];
+            if check == Cell::Empty {
+                continue;
+            }
+            let double = [self.cells[i + 3], self.cells[i + 6]];
+            if double.iter().all(|&cell| cell == check) {
+                return true;
+            }
+        }
+        false
+    }
+
+    // Returns true if a diagonal is complete.
+    fn check_diagonals(&self) -> bool {
+        let check = self.cells[0];
+        if check != Cell::Empty {
+            let double = [self.cells[4], self.cells[8]];
+            if double.iter().all(|&cell| cell == check) {
+                return true;
+            }
+            let check = self.cells[2];
+            if check != Cell::Empty {
+                let double = [self.cells[4], self.cells[6]];
+                if double.iter().all(|&cell| cell == check) {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+
+    fn player_has_won(&self) -> bool {
+        self.check_rows() || self.check_columns() || self.check_diagonals()
+    }
+
+    fn is_full(&self) -> bool {
+        self.turn == GRID_SIZE
+    }
 }
 
 #[no_mangle]
 extern "C" fn sample_main() {
     let mut comm = io::Comm::new();
 
-    let mut x_pos: i32 = 0;
-    let mut y_pos = 0;
-    let bitmap = [0xFF, 0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00, 0xFF, 0xA5, 0xA5];
+    let mut grid = Grid::new();
+    grid.draw();
 
     loop {
         match comm.next_event() {
             io::Event::Button(ButtonEvent::LeftButtonPress) => {
-                calculate_new_position(&mut x_pos, &mut y_pos, ButtonEvent::LeftButtonPress);
-                screen_clear_and_draw(x_pos, y_pos, &bitmap);
+                grid.select_prev();
             }
             io::Event::Button(ButtonEvent::RightButtonPress) => {
-                calculate_new_position(&mut x_pos, &mut y_pos, ButtonEvent::RightButtonPress);
-                screen_clear_and_draw(x_pos, y_pos, &bitmap);
+                grid.select_next();
+            }
+            io::Event::Button(ButtonEvent::BothButtonsRelease) => {
+                grid.select_next();
+                grid.add_mark();
+                if grid.player_has_won() || grid.is_full() {
+                    nanos_sdk::exit_app(0);
+                }
             }
             io::Event::Command::<u8>(_) => (),
             _ => (),

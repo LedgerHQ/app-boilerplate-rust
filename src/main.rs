66fb798e32e65fd62e04f24c792ea7fa5909b174
pscott <scott.piriou@ledger.fr>
2021-06-23 10:19:49 +0200

    Remove dead code

--- src/main.rs
+++ src/main.rs
@@ -4,81 +4,11 @@
 mod crypto_helpers;
 mod utils;
 
-use core::str::from_utf8;
-use crypto_helpers::*;
 use nanos_sdk::buttons::ButtonEvent;
-use nanos_sdk::ecc::DerEncodedEcdsaSignature;
 use nanos_sdk::io;
-use nanos_sdk::io::SyscallError;
 use nanos_sdk::screen;
-use nanos_ui::ui;
 
 nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
-
-/// Display public key in two separate
-/// message scrollers
-fn show_pubkey() {
-    let pubkey = get_pubkey();
-    match pubkey {
-        Ok(pk) => {
-            {
-                let hex0 = utils::to_hex(&pk.W[1..33]).unwrap();
-                let m = from_utf8(&hex0).unwrap();
-                ui::MessageScroller::new(&m).event_loop();
-            }
-            {
-                let hex1 = utils::to_hex(&pk.W[33..65]).unwrap();
-                let m = from_utf8(&hex1).unwrap();
-                ui::MessageScroller::new(&m).event_loop();
-            }
-        }
-        Err(_) => ui::popup("Error"),
-    }
-}
-
-/// Basic nested menu. Will be subject
-/// to simplifications in the future.
-fn menu_example() {
-    loop {
-        match ui::Menu::new(&[&"PubKey", &"Infos", &"Back", &"Exit App"]).show() {
-            0 => show_pubkey(),
-            1 => loop {
-                match ui::Menu::new(&[&"Copyright", &"Authors", &"Back"]).show() {
-                    0 => ui::popup("2020 Ledger"),
-                    1 => ui::popup("???"),
-                    _ => break,
-                }
-            },
-            2 => return,
-            3 => nanos_sdk::exit_app(0),
-            _ => (),
-        }
-    }
-}
-
-/// This is the UI flow for signing, composed of a scroller
-/// to read the incoming message, a panel that requests user
-/// validation, and an exit message.
-fn sign_ui(message: &[u8]) -> Result<Option<DerEncodedEcdsaSignature>, SyscallError> {
-    ui::popup("Message review");
-
-    {
-        let hex = utils::to_hex(&message).map_err(|_| SyscallError::Overflow)?;
-        let m = from_utf8(&hex).map_err(|_| SyscallError::InvalidParameter)?;
-
-        ui::MessageScroller::new(&m).event_loop();
-    }
-
-    if ui::Validator::new("Sign ?").ask() {
-        let k = get_private_key()?;
-        let (sig, _sig_len) = detecdsa_sign(&message, &k).unwrap();
-        ui::popup("Done !");
-        Ok(Some(sig))
-    } else {
-        ui::popup("Cancelled");
-        Ok(None)
-    }
-}
 
 fn screen_clear() {
     screen::sdk_bagl_hal_draw_rect(0, 0, 0, 128, 64);
@@ -127,17 +57,7 @@ extern "C" fn sample_main() {
     let bitmap = [0xFF, 0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00, 0xFF, 0xA5, 0xA5];
 
     loop {
-        // Draw some 'welcome' screen
-        // ui::SingleMessage::new("W e l c o m e").show();
-
-        // Wait for either a specific button push to exit the app
-        // or an APDU command
         match comm.next_event() {
-            // io::Event::Button(ButtonEvent::RightButtonRelease) => nanos_sdk::exit_app(0),
-            // io::Event::Command(ins) => match handle_apdu(&mut comm, ins) {
-            //     Ok(()) => comm.reply_ok(),
-            //     Err(sw) => comm.reply(sw),
-            // },
             io::Event::Button(ButtonEvent::LeftButtonPress) => {
                 calculate_new_position(&mut x_pos, &mut y_pos, ButtonEvent::LeftButtonPress);
                 screen_clear_and_draw(x_pos, y_pos, &bitmap);
@@ -146,56 +66,8 @@ extern "C" fn sample_main() {
                 calculate_new_position(&mut x_pos, &mut y_pos, ButtonEvent::RightButtonPress);
                 screen_clear_and_draw(x_pos, y_pos, &bitmap);
             }
-            io::Event::Command(ins) => match handle_apdu(&mut comm, ins) {
-                Ok(()) => comm.reply_ok(),
-                Err(sw) => comm.reply(sw),
-            },
-
+            io::Event::Command::<u8>(_) => (),
             _ => (),
         }
     }
-}
-
-#[repr(u8)]
-enum Ins {
-    GetPubkey,
-    Sign,
-    Menu,
-    ShowPrivateKey,
-    Exit,
-}
-
-impl From<u8> for Ins {
-    fn from(ins: u8) -> Ins {
-        match ins {
-            2 => Ins::GetPubkey,
-            3 => Ins::Sign,
-            4 => Ins::Menu,
-            0xfe => Ins::ShowPrivateKey,
-            0xff => Ins::Exit,
-            _ => panic!(),
-        }
-    }
-}
-
-use nanos_sdk::io::Reply;
-
-fn handle_apdu(comm: &mut io::Comm, ins: Ins) -> Result<(), Reply> {
-    if comm.rx == 0 {
-        return Err(io::StatusWords::NothingReceived.into());
-    }
-
-    match ins {
-        Ins::GetPubkey => comm.append(&get_pubkey()?.W),
-        Ins::Sign => {
-            let out = sign_ui(comm.get_data()?)?;
-            if let Some(o) = out {
-                comm.append(&o)
-            }
-        }
-        Ins::Menu => menu_example(),
-        Ins::ShowPrivateKey => comm.append(&bip32_derive_secp256k1(&BIP32_PATH)?),
-        Ins::Exit => nanos_sdk::exit_app(0),
-    }
-    Ok(())
 }

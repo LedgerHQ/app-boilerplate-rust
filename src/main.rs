45aca14d56e982c1b938cf44f3e41025f05c1563
pscott <scott.piriou@ledger.fr>
2021-07-04 14:20:12 +0200

    Add sources for snake

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,33 @@
 #![no_std]
 #![no_main]
 
-use core::f32::MAX;
-
 use nanos_sdk::buttons::ButtonEvent;
-use nanos_sdk::exit_app;
 use nanos_sdk::io;
 use nanos_sdk::screen;
-mod grid;
-mod logos;
-use grid::Grid;
-use logos::*;
+
+mod snake;
+
+use snake::Game;
 
 nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
-
-const MAX_TICKER: u16 = 60;
 
 fn screen_clear() {
     screen::sdk_bagl_hal_draw_rect(0, 0, 0, 128, 64);
     screen::sdk_screen_update();
 }
 
-fn screen_clear_and_draw(x_pos: i32, y_pos: i32, width: u32, height: u32, bmp: &[u8]) {
-    screen_clear();
-    screen::sdk_bagl_hal_draw_bitmap_within_rect(x_pos, y_pos, width, height, false, bmp);
-    screen::sdk_screen_update();
-}
-
-#[allow(clippy::manual_range_contains)]
-fn get_next_bmp(cnt: u16) -> &'static Logo {
-    if cnt < MAX_TICKER / 2 {
-        &LOGO_PASCAL
-    } else if cnt < 6 * MAX_TICKER / 10 {
-        &LOGO_RUSTX_0
-    } else if cnt < 7 * MAX_TICKER / 10 {
-        &LOGO_RUSTX_1
-    } else if cnt < 8 * MAX_TICKER / 10 {
-        &LOGO_RUSTX_0
-    } else if cnt < 9 * MAX_TICKER / 10 {
-        &LOGO_RUSTX_1
-    } else {
-        &LOGO_RUSTX_0
-    }
-}
-
 #[no_mangle]
 extern "C" fn sample_main() {
     let mut comm = io::Comm::new();
 
-    let mut grid = Grid::new();
-    grid.init_draw();
-
-    let x_pos: i32 = 0;
-    let y_pos = 0;
-
-    let width: u32 = 128;
-    let height: u32 = 64;
-
-    let mut cnt = 0;
-    let mut current: &'static Logo = &LOGO_RUSTX_1;
+    let mut game = Game::new();
 
     loop {
         match comm.next_event() {
-            io::Event::Button(ButtonEvent::LeftButtonRelease) => {
-                grid.select_prev();
-            }
-            io::Event::Button(ButtonEvent::RightButtonRelease) => {
-                grid.select_next();
-            }
-            io::Event::Button(ButtonEvent::BothButtonsRelease) => {
-                if grid.is_finished() {
-                    exit_app(0);
-                }
-                grid.add_mark();
-            }
-            io::Event::Ticker => {
-                // Only display message if game is finished.
-                if grid.is_finished() {
-                    if cnt == MAX_TICKER {
-                        cnt = 0;
-                    }
-                    cnt += 1;
-                    let bmp = get_next_bmp(cnt);
-                    // Only display screen if it has changed.
-                    if bmp != current {
-                        current = bmp;
-                        screen_clear_and_draw(x_pos, y_pos, width, height, bmp);
-                    }
-                }
-            }
+            io::Event::Button(ButtonEvent::LeftButtonRelease) => game.turn_left(),
+            io::Event::Button(ButtonEvent::RightButtonRelease) => game.turn_right(),
+            io::Event::Button(ButtonEvent::BothButtonsRelease) => {}
+            io::Event::Ticker => game.step(),
             io::Event::Command::<u8>(_) => (),
             _ => (),
         }
---
+++ src/snake.rs
@@ -0,0 +1,211 @@
+use crate::screen_clear;
+use nanos_sdk::screen::sdk_bagl_hal_draw_rect;
+use nanos_sdk::screen::sdk_screen_update;
+
+const BLACK: u32 = 0;
+const WHITE: u32 = 1;
+
+const SCREEN_WIDTH: i32 = 127;
+const SCREEN_HEIGHT: i32 = 64;
+
+const SNAKE_MAX_SIZE: u8 = 200;
+
+/// Game representation.
+pub struct Game {
+    snake: Snake,
+    target: Point,
+}
+
+impl Game {
+    /// An iteration in the game. Designed to be call on every ticker event.
+    pub fn step(&mut self) {
+        // Clear the snake from the screen.
+        self.snake.body[..self.snake.size]
+            .iter()
+            .for_each(|point| sdk_bagl_hal_draw_rect(BLACK, point.x, point.y, 1, 1));
+
+        // Advance the snake.
+        self.snake.step(self.target);
+
+        // Check if the target was eaten by the snake.
+        if self.snake.eaten {
+            // Target was eaten, generate a new target.
+            let mut target = Point::random();
+            // Check that generated target is not IN the snake's body. If it is, generate a new one...
+            while self.snake.body.iter().any(|&point| point == target) {
+                target = Point::random();
+            }
+
+            // Update the target
+            self.target = target;
+            // Draw the target on screen.
+            sdk_bagl_hal_draw_rect(WHITE, target.x, target.y, 1, 1);
+        }
+
+        // Draw the snake in white.
+        self.snake.body[..self.snake.size]
+            .iter()
+            .for_each(|point| sdk_bagl_hal_draw_rect(WHITE, point.x, point.y, 1, 1));
+
+        sdk_screen_update();
+    }
+
+    /// Create a new game.
+    pub fn new() -> Self {
+        screen_clear();
+
+        let target = Point::random();
+        sdk_bagl_hal_draw_rect(WHITE, target.x, target.y, 1, 1);
+        sdk_screen_update();
+
+        Self {
+            snake: Snake::new(),
+            target,
+        }
+    }
+
+    pub fn turn_right(&mut self) {
+        self.snake.direction = match self.snake.direction {
+            Direction::Left => Direction::Down,
+            Direction::Up => Direction::Left,
+            Direction::Right => Direction::Up,
+            Direction::Down => Direction::Right,
+        }
+    }
+
+    pub fn turn_left(&mut self) {
+        self.snake.direction = match self.snake.direction {
+            Direction::Left => Direction::Up,
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+        }
+    }
+}
+
+#[derive(Default, Clone, Copy, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    /// Creates a {0, 0} point.
+    const fn zero() -> Self {
+        Self { x: 0, y: 0 }
+    }
+
+    /// Generates a random point. This point will be WITHIN the screen.
+    fn random() -> Self {
+        let mut x = nanos_sdk::random::rand_u8() as i32;
+        let width = 0..SCREEN_WIDTH;
+        // make sure X is in the screen.
+        while !width.contains(&x) {
+            x = nanos_sdk::random::rand_u8() as i32;
+        }
+
+        let height = 0..SCREEN_HEIGHT;
+
+        let mut y = nanos_sdk::random::rand_u8() as i32;
+
+        // make sure Y is in the screen.
+        while !height.contains(&y) {
+            y = nanos_sdk::random::rand_u8() as i32;
+        }
+
+        Self { x, y }
+    }
+}
+
+/// Direction of the snake.
+enum Direction {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+
+pub struct Snake {
+    body: [Point; SNAKE_MAX_SIZE as usize],
+    size: usize,
+    direction: Direction,
+    eaten: bool,
+}
+
+impl Snake {
+    /// Creates a new snake. The snake starts at a random position, and will go towards the right.
+    fn new() -> Self {
+        let mut body = [Point::zero(); SNAKE_MAX_SIZE as usize];
+        body[0] = Point::random();
+
+        Self {
+            body,
+            size: 1,
+            direction: Direction::Right,
+            eaten: false,
+        }
+    }
+
+    /// Perform a single step for the snake.
+    /// Check `snake.eaten` after `step` to see if the snake ate the target during this step.
+    fn step(&mut self, target: Point) {
+        // Reset the eaten flag to false.
+        self.eaten = false;
+
+        let mut next_head = self.body[0];
+        // Move the next_head.
+        match self.direction {
+            Direction::Right => {
+                if next_head.x == SCREEN_WIDTH {
+                    next_head.x = 0;
+                } else {
+                    next_head.x += 1;
+                }
+            }
+            Direction::Left => {
+                if next_head.x == 0 {
+                    next_head.x = SCREEN_WIDTH;
+                } else {
+                    next_head.x -= 1;
+                }
+            } // check underflow
+            Direction::Down => {
+                if next_head.y == 0 {
+                    next_head.y = SCREEN_HEIGHT;
+                } else {
+                    next_head.y -= 1;
+                }
+            } // check underflow
+            Direction::Up => {
+                if next_head.y == SCREEN_HEIGHT {
+                    next_head.y = 0;
+                } else {
+                    next_head.y += 1;
+                }
+            } // check overflow
+        }
+
+        // Check if we are about to eat the target.
+        if next_head == target {
+            // We ate the target: set the self.eaten flag accordingly.
+            self.eaten = true;
+            // We juste ate the target: grow in size.
+            self.size += 1;
+        }
+
+        // Move the body (limited by `self.size`) on cell to the right.
+        // The last cell will get put in the first cell (wrap around).
+        self.body[..self.size].rotate_right(1);
+
+        // Check if `next_head` is going to collide with the body.
+        if self.body[..self.size]
+            .iter()
+            .any(|&point| point == next_head)
+        {
+            nanos_sdk::exit_app(1);
+        }
+
+        // Set the first cell (the head) to be the `next_head` we computed before.
+        self.body[0] = next_head;
+    }
+}

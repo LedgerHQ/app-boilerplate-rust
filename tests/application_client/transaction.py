36e3dc6d2d028639cdf06aab1c16a156e668fcf8
darwintree <17946284+darwintree@users.noreply.github.com>
2024-11-05 11:04:59 +0800

    fix: serialize returns encoded transaction

--- tests/application_client/transaction.py
+++ tests/application_client/transaction.py
@@ -1,8 +1,5 @@
-import json
-from dataclasses import dataclass
-from .utils import UINT64_MAX
 from cfx_utils.types import TxParam
-from cfx_account._utils.transactions.transactions import Transaction as CfxTransaction
+from cfx_account.transactions.transactions import Transaction as CfxTransaction
 
 
 class TransactionError(Exception):
@@ -10,7 +7,8 @@ class TransactionError(Exception):
 
 class Transaction:
     def __init__(self, **kwargs: TxParam):
+        
         self.tx = CfxTransaction.from_dict(kwargs)
 
     def serialize(self) -> bytes:
-        return self.tx.hash()
+        return self.tx.encode(allow_unsigned=True)

dce2feef291d557e1029929ac557fd8bbf171552
Francois Beutin <francois.beutin@ledger.fr>
2025-12-22 20:21:49 +0100

    CRAB swap v1

--- src/handlers/get_public_key.rs
+++ src/handlers/get_public_key.rs
@@ -16,34 +16,50 @@
  *****************************************************************************/
 
 use crate::app_ui::address::ui_display_pk;
-use crate::utils::Bip32Path;
+use crate::utils::{get_address_hash_from_pubkey, get_pubkey_from_path, Bip32Path};
 use crate::AppSW;
 use ledger_device_sdk::ecc::{Secp256k1, SeedDerive};
-use ledger_device_sdk::hash::{sha3::Keccak256, HashInit};
 use ledger_device_sdk::io::Comm;
 
+/// Handler for GET_PUBLIC_KEY APDU command.
+///
+/// Derives and returns the public key for a given BIP32 path, optionally
+/// displaying the corresponding address on the device for user verification.
+///
+/// # Flow
+///
+/// 1. Parse BIP32 path from APDU data
+/// 2. Derive public key using shared helper `get_pubkey_from_path()`
+/// 3. If display requested, compute and show address on device
+/// 4. Return public key and chaincode to client
+///
+/// # Note
+///
+/// This handler uses the same address derivation logic as `swap::check_address()`
+/// via the shared `get_address_hash_from_pubkey()` helper, ensuring consistency.
 pub fn handler_get_public_key(comm: &mut Comm, display: bool) -> Result<(), AppSW> {
     let data = comm.get_data().map_err(|_| AppSW::WrongApduLength)?;
     let path: Bip32Path = data.try_into()?;
 
-    let (k, cc) = Secp256k1::derive_from(path.as_ref());
-    let pk = k.public_key().map_err(|_| AppSW::KeyDeriveFail)?;
+    // Derive public key using shared helper (also used by swap)
+    let pubkey = get_pubkey_from_path(&path)?;
+    let (_, cc) = Secp256k1::derive_from(path.as_ref());
 
     // Display address on device if requested
     if display {
-        let mut keccak256 = Keccak256::new();
-        let mut address: [u8; 32] = [0u8; 32];
+        // Compute address using shared helper (same as swap::check_address)
+        let address_hash = get_address_hash_from_pubkey(&pubkey);
 
-        let _ = keccak256.hash(&pk.pubkey[1..], &mut address);
-
-        if !ui_display_pk(&address)? {
+        if !ui_display_pk(&address_hash)? {
             return Err(AppSW::Deny);
         }
     }
 
-    comm.append(&[pk.pubkey.len() as u8]);
-    comm.append(&pk.pubkey);
+    // Return public key to client (65 bytes uncompressed)
+    comm.append(&[pubkey.len() as u8]);
+    comm.append(&pubkey);
 
+    // Return chaincode
     const CHAINCODE_LEN: u8 = 32;
     let code = cc.unwrap();
     comm.append(&[CHAINCODE_LEN]);
---
+++ tests/swap/test_boilerplate.py
@@ -0,0 +1,85 @@
+import pytest
+from ledger_app_clients.exchange.test_runner import ExchangeTestRunner, ALL_TESTS_EXCEPT_MEMO_THORSWAP_AND_FEES
+from ledger_app_clients.exchange.cal_helper import CurrencyConfiguration
+from ragger.error import ExceptionRAPDU
+from ragger.utils import create_currency_config
+
+from application_client.boilerplate_currency_utils import BOL_PATH
+from application_client.boilerplate_command_sender import BoilerplateCommandSender, Errors as BoilerplateErrors
+from application_client.boilerplate_transaction import Transaction
+
+from . import cal_helper as cal
+
+
+# Token addresses from the hardcoded database in src/token/token_db.c
+# USDC token with 12 decimals
+TOKEN_USDC_ADDRESS = "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+TOKEN_LINK_ADDRESS = "ffeeddccbbaa99887766554433221100ffeeddccbbaa99887766554433221100"
+UNKNOWN_TOKEN_ADDRESS = "00ffeeddccbbaa99887766554433221100ffeeddccbbaa998877665544332211"
+
+# CAL dynamic token address (not in hardcoded database, will be provided dynamically)
+TOKEN_DYNAMIC_USDT_ADDRESS = cal.TOKEN_DYNAMIC_USDT_ADDRESS
+TOKEN_HARDCODED_USDC_ADDRESS = cal.TOKEN_HARDCODED_USDC_ADDRESS
+
+
+# ExchangeTestRunner implementation for Boilerplate.
+# BoilerplateTests extends ExchangeTestRunner and in doing so inherits all necessary boilerplate and
+# test management features.
+# We only need to set the values we want to use for our test and the final payment request
+class BoilerplateTests(ExchangeTestRunner):
+    # The coin configuration of our currency. Replace by your own
+    currency_configuration = cal.BOL_CURRENCY_CONFIGURATION
+    # A valid template address of a supposed trade partner.
+    valid_destination_1 = "de0b295669a9fd93d5f28d9ec85e40f4cb697bae"
+    # A memo to use associated with the destination address if applicable.
+    valid_destination_memo_1 = ""
+    # A second valid template address of a supposed trade partner.
+    valid_destination_2 = "9fc3da866e7dF3a1c57ade1a97c9f00a70f010c8"
+    # A second memo to use associated with the destination address if applicable.
+    valid_destination_memo_2 = ""
+    # The address of the Speculos seed on the BOL_PATH.
+    valid_refund = "fd2095a37e72be2cd575d18fe8f16e78c51eafa3"
+    valid_refund_memo = ""
+
+    # Values we ask the ExchangeTestRunner to use in the test setup
+    valid_send_amount_1 = 1000
+    valid_send_amount_2 = 666
+    valid_fees_1 = 0
+    valid_fees_2 = 0
+
+    # Fake addresses to test the address rejection code.
+    fake_refund = "abcdabcd"
+    fake_refund_memo = "bla"
+    fake_payout = "abcdabcd"
+    fake_payout_memo = "bla"
+
+    # The error code we expect our application to respond when encountering errors.
+    signature_refusal_error_code = BoilerplateErrors.SW_DENY
+    wrong_amount_error_code = BoilerplateErrors.SW_SWAP_FAIL
+    wrong_destination_error_code = BoilerplateErrors.SW_SWAP_FAIL
+
+    # The final transaction to craft and send as part of the SWAP finalization.
+    # This function will be called by the ExchangeTestRunner in a callback like way
+    def perform_final_tx(self, destination, send_amount, fees, memo):
+        # Create the transaction that will be sent to the device for signing
+        tx = Transaction(
+            nonce=1,
+            coin="CRAB",
+            to=destination,
+            value=send_amount,
+            memo=memo
+        ).serialize()
+
+        # Send the TX
+        BoilerplateCommandSender(self.backend).sign_tx_sync(path=BOL_PATH, transaction=tx)
+
+        # TODO : assert signature validity. Not required but recommended
+
+
+# We use a class to reuse the same Speculos instance (faster performances)
+class TestsBoilerplate:
+    # Run all the tests applicable to our setup: here we don't test fees mismatch, memo mismatch, and Thorswap / LiFi
+    @pytest.mark.parametrize('test_to_run', ALL_TESTS_EXCEPT_MEMO_THORSWAP_AND_FEES)
+    def test_boilerplate(self, backend, exchange_navigation_helper, test_to_run):
+        # Call run_test method of ExchangeTestRunner
+        BoilerplateTests(backend, exchange_navigation_helper).run_test(test_to_run)
